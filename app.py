ed8c0884888309c5646baa3d2fc7b90fbcfe2dc3
renge-bharati <rengebharati@gmail.com>
2025-12-11 17:48:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title("📘 Student Score Prediction App")
 st.write("Enter study hours to predict expected exam score.")
 
 # Load model
-model = joblib.load("model.pkl")
+model = load("model.pkl")
 
 # User input
 hours = st.number_input("Enter Hours Studied:", min_value=0.0, max_value=24.0, step=0.5)
